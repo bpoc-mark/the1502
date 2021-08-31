c8b6d2106682c700d20e07f795e3c5195c49271e
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-31 17:47:09 +0800

    add google map

--- inc/top/section_6.php
+++ inc/top/section_6.php
@@ -1,9 +1,7 @@
 <section class="sect_6" id="sect_6">
     <div class="mapouter">
         <div class="gmap_canvas">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.473385433963!2d135.95993911524076!3d35.019885480353764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001723fee6f47df%3A0x2309dfdaf40065f6!2z5Yi66Lqr44Go6a2a6aOvIEZVTkVZQQ!5e0!3m2!1sja!2sph!4v1624005123492!5m2!1sja!2sph"
-                width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5615.650528755384!2d135.95705983450773!3d35.01831538798107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001736dd1efa777%3A0xedbb51cca8601456!2sGyokai-to%20Shirowain%20The1502!5e0!3m2!1sen!2sph!4v1630402982401!5m2!1sen!2sph" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     <div>
 </section>
