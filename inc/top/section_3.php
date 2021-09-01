c349e591f4a013b8240a681b8025baea39f97948
denver <denver.gomez@bpoc.co.jp>
2021-09-01 09:43:21 +0800

    update instagram content

--- inc/top/section_3.php
+++ inc/top/section_3.php
@@ -1,4 +1,4 @@
-<section class="sect_3" >
+<section class="sect_3">
     <div class="outer_container">
         <div class="l-container sect_3__inner">
             <div class="l-container bg-overlay ml-13">
@@ -22,10 +22,14 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>２年前</span>
+                            <span>3 週間前 <small>新規</small></span>
                         </div>
-                        <p class="c-rating">接客も料理も高レベルだと思います。<br>
-                            毎回満足です。</p>
+                        <p class="c-rating">美味しい料理とお酒<img
+                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/liquior_icon.png"
+                                alt=""><br>
+                            楽しい時間で時がたつのをすぐ忘れてしまいます<img
+                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_icon.png"
+                                alt=""></p>
                     </div>
                     <!-- Row Star 2 -->
                     <div class="border-b c-pad">
@@ -40,13 +44,26 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>４か月前</span>
+                            <span>6か月前</span>
                         </div>
-                        <p class="c-rating">お店として手に入る素材で頑張ってる印象を受けました。<br>
-                            好感が持てました。</p>
-                        <div class="c-food"> <img class="food-1"
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/food_1.png"
+                        <p class="c-rating">
+                            雰囲気の良いお店ですお料理も一つ一つか凝っていて目でも楽しめるモノばかりです♪お野菜もお魚も新鮮で本当に美味しいお料理ばかり、フネヤパフェも是非食べてもらいたいもらいたいお勧めデザートです<br>
+                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/heart_icon_w.png"
                                 alt="">
+                        </p>
+                        <div class="inline-img">
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_1.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_2.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_3.png"
+                                    alt="">
+                            </div>
                         </div>
                     </div>
                     <!-- Row Star 3 -->
@@ -62,14 +79,50 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>１年前</span>
+                            <span>4 か月前</span>
                         </div>
-                        <p class="c-rating">お料理はおいしいです。<br>
-                            意外に奥行きのある店舗でたくさん入れます。<br>
-                            間仕切りは暖簾ですし、静かに食事というよりはわいわいがやがやでいい場合に向いています。</p>
+                        <p class="c-rating">連休中だったのですが運良く入れました<img
+                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_close_eye_icon.png"
+                                alt=""><br>
+                            お店の方達がとっても感じが良くて、旅の楽しさがましました<img
+                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/heart_icon_r.png"
+                                alt=""><br>
+                            <br>
+                            土鍋の鯛めしが絶品<span>!</span>最後に出汁をかけて食べる食べ方は初めてでした。広島の牡蠣も生臭くなく一人前の料理も多い、そして契約農家さんが作っている野菜が美味しかった。<br>
+                            <br>
+                            料理も工夫があり、感じもいいし、良い店見つけました<img
+                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_close_eye_icon.png"
+                                alt="">
+                        </p>
+                        <div class="inline-img">
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_4.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_5.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_6.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_7.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_8.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_9.png"
+                                    alt="">
+                            </div>
+                        </div>
                     </div>
                     <!-- Row Star 4 -->
-                    <div class="border-b c-pad">
+                    <div class="c-pad">
                         <div class="star-content">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
@@ -81,9 +134,24 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>１年前</span>
+                            <span>2 年前</span>
                         </div>
-                        <p class="c-rating">何よりも日本酒の種類が多いのでいいですね。</p>
+                        <p class="c-rating">前にあった守山の頃から通って10年位になります。<br>
+                            海のない滋賀県でもこれだけ新鮮な海の幸が食べれるのはここくらいじゃないでしょうか。<br>
+                            経営者のマスターを筆頭にアルバイトも美男美女が揃っています。<br>
+                            安くて美味しく楽しいお店です。<br>
+                            これ以上混み合って欲しくないので本当は教えたくないんですがww
+                            予約する事をお勧めします。</p>
+                        <div class="inline-img">
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_10.png"
+                                    alt="">
+                            </div>
+                            <div> <img class="food-1"
+                                    src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_11.png"
+                                    alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
