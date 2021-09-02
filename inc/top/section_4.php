70812e4d7d35754b1977d890f26dcf829b134ab9
arsua-greg <gregarsua@bpoc.co.jp>
2021-09-02 09:49:17 +0800

    replace insta

--- inc/top/section_4.php
+++ inc/top/section_4.php
@@ -4,35 +4,35 @@
 
 
             <?php
-                $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+            $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
 
-                $args = array(
-                    'post_type' => 'post',
-                    'post_status' => 'publish',
-                    'posts_per_page' => 4,
-                    'category_name' => 'Instagram',
-                    'paged' => $paged,
-                );
+            $args = array(
+                'post_type' => 'post',
+                'post_status' => 'publish',
+                'posts_per_page' => 4,
+                'category_name' => 'Instagram',
+                'paged' => $paged,
+            );
 
-                $the_query = new WP_Query($args);
-                ?>
+            $the_query = new WP_Query($args);
+            ?>
 
-                <?php if ($the_query->have_posts()) : ?>
+            <?php if ($the_query->have_posts()) : ?>
 
-                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
-                        <div class="l-insta__content-1">
-                            <?php the_content(); ?>
-                        </div>
+                    <div class="l-insta__content-1">
+                        <?php the_content(); ?>
+                    </div>
 
-                    <?php endwhile; ?>
+                <?php endwhile; ?>
             <?php endif; ?>
 
         </div>
         <div class="l-content l-insta__offcl-inst">
-            <a class="off-text off-text-primary" href="https://www.instagram.com/funeya_kusatsu/" target="_blank"> 
-            <img class="insta-pic" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/insta-logo-white.png" alt="">
-                &nbsp;official Instagram : funeya_kusatsu</a>
+            <a class="off-text off-text-primary" href="https://www.instagram.com/the1502_shiga/" target="_blank">
+                <img class="insta-pic" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_4/insta-logo-white.png" alt="">
+                &nbsp;official Instagram : the1502_shiga</a>
         </div>
     </div>
 </section>
